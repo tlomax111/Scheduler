3b1d73261c9832546f97db51f488d2cf38263efb
Toni Lomax <lomaxtoni@aol.com>
2020-02-14 18:10:45 -0500

    adding corrected files

--- Scheduler.js
+++ Scheduler.js
@@ -1,46 +1,58 @@
-var todo = [{
-    "9am": "",
-    "10am": "",
-    "11am": "",
-    "12am": "",
-    "1pm": "",
-    "2pm": "",
-    "3pm": "",
-    "4pm": "",
-    "5pm": "",
-    "6pm": "",
-    "7pm": "",
-    "8pm": "",
-    "9pm": "",
-}]
+// var todo = [{
+//     "9am": "",
+//     "10am": "",
+//     "11am": "",
+//     "12am": "",
+//     "1pm": "",
+//     "2pm": "",
+//     "3pm": "",
+//     "4pm": "",
+//     "5pm": "",
+//     "6pm": "",
+//     "7pm": "",
+//     "8pm": "",
+//     "9pm": "",
+// }]
 
 //user types task
 var userInput1 = document.getElementById("textCol1");
 var saveInput1 = localStorage.getItem("textCol1");
 
 
-//clicks save and input gets saved to storage
-
 
 $("button").click(function () {
-    $(this).click();
+    
     // var userInput1 = document.getElementById("textCol1");
-    var userInput1 = $("#textCol1")
-    console.log("userInput1.val()", userInput1.val())
+    var userInputArr = $(this).prev()
+
+    console.log(userInputArr)
+    // console.log("userInput1.val()", userInputArr[0].val()
+    
+    var userInputText = $(userInputArr[0]).val()
+
+    console.log(userInputArr[0].id)
+
+    var id = userInputArr[0].id
     // localStorage.setItem("userInput1", userInput1.value);
-    localStorage.setItem("userInput1", userInput1.val());
-  
+    localStorage.setItem(id, userInputText);
+
+    sessionStorage.setItem(id, userInputText);
+    
+    if (sessionStorage){
+        
+
+    }
 });
 
-  
-    if (localStorage.getItem("textCol1") || JSON.parse(localStorage.getItem("textCol")).length === 0) {
-    $window.localStorage.setItem("textCol1", JSON.stringify($scope.textCol1));}
-    
+//Sets a Day/Time
+var d = new Date();
+document.getElementById("currentDay").innerHTML = d;
 
+// console.log(JSON.parse(localStorage.getItem("textCol")))
 
+//get the id you want to append to and get the value of the 
 
-
-console.log(userInput1)
+// console.log(userInput1)
 //time clock
 
 
